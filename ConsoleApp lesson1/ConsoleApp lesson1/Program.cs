908ae00dec1a47eef04bbea5bf527afb3c27ebf6
temitope2030@outlook.com <temitope2030@outlook.com>
2020-06-09 15:03:23 -0500

    This is my first commit.

---
+++ ConsoleApp lesson1/ConsoleApp lesson1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp_lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a lesson");
+        }
+    }
+}
