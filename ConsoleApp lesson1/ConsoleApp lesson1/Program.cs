80721ef09a31fec63973322b9cd4c14b95efeafa
temitope2030@outlook.com <temitope2030@outlook.com>
2020-06-11 20:54:16 -0500

    The values are big.

--- ConsoleApp lesson1/ConsoleApp lesson1/Program.cs
+++ ConsoleApp lesson1/ConsoleApp lesson1/Program.cs
@@ -5,10 +5,12 @@ namespace ConsoleApp_lesson1
     class Program
     {
         static void Main(string[] args)
+
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("This is a lesson");
             Console.WriteLine("This is my second lesson");
+            Console.WriteLine("The values are big");
         }
     }
 }
