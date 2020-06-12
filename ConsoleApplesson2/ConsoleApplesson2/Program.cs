84c0a51a1348d43e76d86dc949567d27885050f0
temitope2030@outlook.com <temitope2030@outlook.com>
2020-06-11 21:15:19 -0500

    This is the platform.

---
+++ ConsoleApplesson2/ConsoleApplesson2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApplesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is lesson2");
+            Console.WriteLine("This is lesson2 change");
+            Console.WriteLine("This is the platform ");
+        }
+    }
+}
