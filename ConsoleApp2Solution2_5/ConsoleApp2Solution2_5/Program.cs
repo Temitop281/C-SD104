9e75e9987b102c6ec5d40e07a2ed1f2fefe15cf5
temitope2030@outlook.com <temitope2030@outlook.com>
2020-06-18 19:55:03 -0500

    Lab3_4

---
+++ ConsoleApp2Solution2_5/ConsoleApp2Solution2_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp2Solution2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int number1; //input one
+            int number2; //input two
+            Console.WriteLine("The answer when multiplying is:");
+            Console.WriteLine(result);
+
+            double result2 = (double) number1 / (double) number2;
+            Console.WriteLine("The answer when dividing is:");
+            Console.WriteLine(result2);
+
+            Console.WriteLine("please enter your name.");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Hello" + userName);
+
+
+            Console.WriteLine("Lets multiply! please enter a number");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter a second number.");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter a third number.");
+            int number3 = Convert.ToInt32(Console.ReadLine());
+            result = number1 * number2 * number3;
+            Console.WriteLine()
+
+
+
+     
+
+        }
+    }
+}
+C
---
+++ ConsoleApplab3_4/ConsoleApplab3_4/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApplab3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[d-e]";
+            string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy...";
+            RegexOptions options = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(input, pattern, options))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+            }
+
+
+
+            string patternb = @"[d-e][*d]";
+            string inputb = @" Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+            RegexOptions optionsb = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(inputb, patternb, optionsb))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+            }
+
+
+
+            string patternc = @"[a][a-z][a]";
+            string inputc = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy...";
+            RegexOptions optionsc = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(inputc, patternc, optionsc))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+
+            }
+
+
+
+
+            string patterne = @"[ ][p][a-z]*";
+            string inpute = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+            RegexOptions optionse = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(inpute, patterne, optionse))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+
+            }
+
+
+
+            string patternf = @"[a-z]*[e][ ]";
+            string inputf = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+            RegexOptions optionsf = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(inputf, patternf, optionsf))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+
+            }
+            string patterng = @"[A-Z]";
+            string inputg = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+            RegexOptions optionsg = RegexOptions.Multiline;
+
+            foreach (Match m in Regex.Matches(inputg, patterng, optionsg))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+      
+        }
+}
