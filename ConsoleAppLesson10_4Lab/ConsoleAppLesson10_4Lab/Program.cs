61493ed836f95d21bd1401f427656ecba0238ee1
temitope2030@outlook.com <temitope2030@outlook.com>
2020-07-14 06:55:06 -0500

    Lab10_4AudioMediaPlayer

---
+++ ConsoleAppLesson10_4Lab/ConsoleAppLesson10_4Lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//interface that plays Audio Media
+
+namespace ConsoleAppLesson10_4Lab
+{
+    interface IAudioPlayer
+    {
+        void Play();
+    }
+
+}
+        
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
