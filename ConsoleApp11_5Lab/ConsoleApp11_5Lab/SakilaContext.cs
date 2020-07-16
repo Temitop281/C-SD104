686c13322345cfee501db6e4b777bfc5c60dd255
temitope2030@outlook.com <temitope2030@outlook.com>
2020-07-16 14:58:57 -0500

    Lesson11_5LabFilm.CS

---
+++ ConsoleApp11_5Lab/ConsoleApp11_5Lab/SakilaContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+
+using System.Text;
+
+namespace ConsoleApp11_5Lab
+{
+    class SakilaContext : DbContext
+    {
+
+        public DbSet<Film> Film { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=localhost\sqlexpress;Database=sakila;Trusted_Connection=True;");
+
+
+
+        }
+    }
+}
